be57e3a3f9105ba7a68e8005d884ecaacc0ae560
TheCrazedLime <thecrazedlime@gmail.com>
2021-05-06 12:21:39 +0200

    add navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from "react-router-dom";
 export default function NavBar() {
   return (
     <div>
-      <NavLink activeStyle={{ fontWeight: "bold" }} to="/" exact>
+      <NavLink activeStyle={{ fontWeight: "bold" }} to="/" exact={true}>
         Home
       </NavLink>
       {" - "}
